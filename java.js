015c9ad65467bbb58faeb64d3dbe46cd62509ec4
Samuel1401s <161243417+Samuel1401s@users.noreply.github.com>
2025-04-08 14:00:08 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    const menu = document.getElementById('menuLateral');
+    menu.classList.toggle('open') }
+
+    let indiceActual = 0;
+
+    function moverCarrusel(direccion) {
+        const carruselItems = document.querySelector('.carrusel-items');
+        const totalItems = carruselItems.children.length;
+        indiceActual += direccion;
+        if (indiceActual < 0) {
+            indiceActual = totalItems - 1;
+        } else if (indiceActual >= totalItems) {
+            indiceActual = 0;
+        }
+        const desplazamiento = -indiceActual * carruselItems.children[0].offsetWidth;
+        carruselItems.style.transform = `translateX(${desplazamiento}px)`;
+    }
