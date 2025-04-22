864c3e7600585fdfb3d9d5002f13d2b607893f78
Samuel1401s <161243417+Samuel1401s@users.noreply.github.com>
2025-04-22 16:37:56 -0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,19 +1,29 @@
 function toggleMenu() {
-    const menu = document.getElementById('menuLateral');
-    menu.classList.toggle('open') }
+    const menu = document.getElementById("menuLateral");
+    menu.classList.toggle("open");
+  }
+  
+  let indiceActual = 0;
+  
+  function moverCarrusel(direccion) {
+    const carruselItems = document.querySelector(".carrusel-items");
+    const imagenes = carruselItems.querySelectorAll("img");
+    const totalItems = imagenes.length;
+   
+    imagenes[indiceActual].classList.remove("active");
+  
+    indiceActual += direccion;
 
-    let indiceActual = 0;
-
-    function moverCarrusel(direccion) {
-        const carruselItems = document.querySelector('.carrusel-items');
-        const totalItems = carruselItems.children.length;
-        indiceActual += direccion;
-        if (indiceActual < 0) {
-            indiceActual = totalItems - 1;
-        } else if (indiceActual >= totalItems) {
-            indiceActual = 0;
-        }
-        const desplazamiento = -indiceActual * carruselItems.children[0].offsetWidth;
-        carruselItems.style.transform = `translateX(${desplazamiento}px)`;
+    if (indiceActual < 0) {
+      indiceActual = totalItems - 1;
+    } else if (indiceActual >= totalItems) {
+      indiceActual = 0;
     }
-    
+  
+    imagenes[indiceActual].classList.add("active");
+  }
+  document.addEventListener("DOMContentLoaded", function() {
+    const carruselItems = document.querySelector(".carrusel-items");
+    const imagenes = carruselItems.querySelectorAll("img");
+    imagenes[0].classList.add("active");
+  });
